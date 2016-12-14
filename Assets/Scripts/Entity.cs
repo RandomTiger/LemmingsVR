93b7d53520799292385b883a7f38fdbc5372a299
RandomTiger <RandomSnowTiger@gmail.com>
2016-12-14 13:53:57 +0000

    Basic setup (without 3rd party tools)

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<CharacterController>().SimpleMove(transform.forward);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Director director = other.GetComponent<Director>();
+		if (director)
+		{
+			transform.Rotate(Vector3.up, director.Turn);
+		}
+	}
+}
